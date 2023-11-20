01aa98dd023ec11893d602c27ce2c761dd8ba577
LexHeadko <n-odes11@yandex.ru>
2023-11-20 14:00:01 +0200

    initial homework commit

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,46 @@ public class Main {
 
     public static void Task1() {
         System.out.println("Task1");
-
+        String tel = "+7(964)-264-37-25";
+        tel = tel.replace("-", "");
+        tel = tel.replace(" ", "");
+        tel = tel.replace("+", "");
+        tel = tel.replace("(", "");
+        tel = tel.replace(")", "");
+        System.out.println("tel = " + tel);
+        if (tel.length() == 10) {
+            tel = '7' + tel;
+        } else if (tel.length() > 11) {
+            throw new RuntimeException("Телефон введен не корректно!");
+        } else if (tel.length() < 10) {
+            throw new RuntimeException("Телефон короткий!");
+        } else if (tel.length() == 11 && tel.charAt(0) != '7') {
+            throw new RuntimeException("Код телефона указан не верно!");
+        }
+        System.out.println("tel = " + tel);
+        String expectedPhone = "79642643725";
+        if (tel.equals(expectedPhone)) {
+            System.out.println("Успех!");
+        } else {
+            System.out.println("Неудача!");
+        }
     }
 
     public static void Task2() {
         System.out.println("Task2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника: " + fullName);
+        System.out.println("ФИО сотрудника для отчета: " + fullName.toUpperCase());
     }
 
     public static void Task3() {
         System.out.println("Task3");
-    }
-
-    public static void Task4() {
-        System.out.println("Task4");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("ФИО сотрудника: " + fullName);
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("ФИО сотрудника: " + fullName);
     }
 }
